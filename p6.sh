da210aaa3e696074eeffbf56b1fa4ee222fdaaaf
RUDRANSH BARADIYA <114794140+RUDRANSHFLY@users.noreply.github.com>
2023-05-06 11:03:28 +0530

    Practical 6 Os of GTU

--- p6.sh
+++ p6.sh
@@ -8,9 +8,14 @@ then
     echo "PLease Enter A Positive Number"
 fi
 
-while [ $n -gt 1 ]
-do
-    i=$((i * n ))
-    n=$(( n -1 ))
+a=1
+b=0
+i=1
+while [ $n -gt $i ]
+do  
+    r=$((a+b));
+    a=$b
+    b=$r
+    echo -n "$r "
+    i=$((i + 1))
 done
-echo "The Factoial of $t is :- " $i
